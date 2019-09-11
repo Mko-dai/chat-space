fb0677946d4334de77309ec72f662b7fefa53c0a
Mko-dai <mk0829@outlook.jp>
2019-09-11 23:26:50 +0900

    first-commit

---
+++ app/assets/javascripts/chat.js
@@ -0,0 +1,47 @@
+$(function(){
+  function buildHTML(chat){
+    var addImage = (chat.image_url !== null) ? `<img src="${chat.image_url}">` : '';
+    var html = `<div id="sounyu" class="contents-right__chats-space__message">
+                <b>${chat.name}</b>
+                <a>${chat.created_at}</a>
+                <br>
+                ${chat.text}
+                </br>
+                ${addImage}
+                </div>`
+return html;
+  };
+  
+  $('#chat').on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    
+      // width: "300px",
+      // height: "300px",
+    
+      // borderColor: "#00f",
+      // backgroundColor: "#f00"
+    
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.contents-right__chats-space').append(html)
+      $('#chat_text').val('')
+      $('.hidden-file-field').val('')
+      $('html,body').animate({scrollTop: $('html,body')[0].scrollHeight}, 'fast');
+      console.log(this);
+    })
+    .fail(function(){
+      alert('error')
+    });
+    return false;
+  });
+});
---
+++ app/views/chats/create.json.jbuilder
@@ -0,0 +1,5 @@
+# binding.pry
+json.text @chat.text
+json.image_url @chat.image.url
+json.name @chat.user.name
+json.created_at @chat.created_at.strftime("%Y/%m/%d %H:%M")
